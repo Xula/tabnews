e21105345a4928ce543d36a9b576c677b11065c0
André Romeu Pedruzzi <andrerpedruzzi@gmail.com>
2025-11-25 00:39:57 +0000

    added external image to homepage

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ const Home = () => {
     return (
         <div>
             <h1>Uma flor pra outra flor</h1>
-            <img src="../assets/cat.jpg" />
+            <img src="https://i.pinimg.com/1200x/4c/42/83/4c428368cc011dc2a442362f0d2bbafd.jpg" />
         </div>
     )
 }
