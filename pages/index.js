0882abf42f5f8b5acc52a8722f0052992fb086af
André Romeu Pedruzzi <andrerpedruzzi@gmail.com>
2025-11-25 21:23:48 +0000

    add lint scripts `lint:check` and `lint:fix` and prettier

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 const Home = () => {
-    return (
-        <div>
-            <h1>Uma flor pra outra flor</h1>
-            <img src="https://i.pinimg.com/1200x/4c/42/83/4c428368cc011dc2a442362f0d2bbafd.jpg" />
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>Uma flor pra outra flor</h1>
+      <img src="https://i.pinimg.com/1200x/4c/42/83/4c428368cc011dc2a442362f0d2bbafd.jpg" />
+    </div>
+  );
+};
 
 export default Home;
