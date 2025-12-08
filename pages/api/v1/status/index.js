7c4508872d2be177342814f6865269d55ffe37c6
André Romeu Pedruzzi <andrerpedruzzi@gmail.com>
2025-12-08 21:04:03 +0000

    add `database.js` e `.env`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-const status = (request, response) => {
+import database from "../../../../infra/database.js";
+
+const status = async (request, response) => {
+  const result = await database.query("SELECT 1+1 as sum;");
+  console.log(result);
   return response.status(200).json({ chave: "xuux de sáás" });
 };
 
